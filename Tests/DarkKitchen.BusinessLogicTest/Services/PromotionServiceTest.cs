dfbd27a913392e8896cb9a6878b9a6593ed8663f
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-01 02:32:04 -0300

    Red: GetActivePromotions_WhenCalled_ReturnsPromotions

---
+++ Tests/DarkKitchen.BusinessLogicTest/Services/PromotionServiceTest.cs
@@ -0,0 +1,37 @@
+using DarkKitchen.BusinessLogic.Services;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Interfaces;
+using Moq;
+
+namespace DarkKitchen.BusinessLogicTest.Services;
+
+[TestClass]
+public sealed class PromotionServiceTest
+{
+    [TestMethod]
+    public void GetActivePromotions_WhenCalled_ReturnsPromotions()
+    {
+        var promotions = new List<Promotion>
+        {
+            new Promotion
+            {
+                Id = 1,
+                Name = "Black Friday",
+                DiscountPercentage = 10,
+                ValidFrom = new DateTime(2026, 1, 25),
+                ValidTo = new DateTime(2026, 1, 30)
+            }
+        };
+
+        var promotionRepositoryMock = new Mock<IPromotionRepository>();
+        promotionRepositoryMock.Setup(r => r.GetActivePromotions(null, null, null))
+                               .Returns(promotions);
+
+        var promotionService = new PromotionService(promotionRepositoryMock.Object);
+
+        var result = promotionService.GetActivePromotions(null, null, null);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Count);
+    }
+}
