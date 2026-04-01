5207481833cb147de9c9c661c2451ee31bcbd05a
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-01 09:35:24 -0300

    dotnet format DarkKitchen.sln

--- DarkKitchen.Domain/Validators/PasswordValidator.cs
+++ DarkKitchen.Domain/Validators/PasswordValidator.cs
@@ -16,7 +16,7 @@ public static class PasswordValidator
 
     private static void ValidateLength(string password)
     {
-        if (password.Length < 15 || password.Length > 25)
+        if(password.Length < 15 || password.Length > 25)
         {
             throw new Exception("La contraseña debe tener entre 15 y 25 caracteres");
         }
@@ -24,7 +24,7 @@ public static class PasswordValidator
 
     private static void ValidateUppercase(string password)
     {
-        if (!Regex.IsMatch(password, @"[A-Z]"))
+        if(!Regex.IsMatch(password, @"[A-Z]"))
         {
             throw new Exception("La contraseña debe tener al menos una mayúscula");
         }
@@ -32,7 +32,7 @@ public static class PasswordValidator
 
     private static void ValidateLowercase(string password)
     {
-        if (!Regex.IsMatch(password, @"[a-z]"))
+        if(!Regex.IsMatch(password, @"[a-z]"))
         {
             throw new Exception("La contraseña debe tener al menos una minúscula");
         }
@@ -40,7 +40,7 @@ public static class PasswordValidator
 
     private static void ValidateNumber(string password)
     {
-        if (!Regex.IsMatch(password, @"[0-9]"))
+        if(!Regex.IsMatch(password, @"[0-9]"))
         {
             throw new Exception("La contraseña debe tener al menos un número");
         }
@@ -48,7 +48,7 @@ public static class PasswordValidator
 
     private static void ValidateSymbol(string password)
     {
-        if (!Regex.IsMatch(password, @"[^a-zA-Z0-9]"))
+        if(!Regex.IsMatch(password, @"[^a-zA-Z0-9]"))
         {
             throw new Exception("La contraseña debe tener al menos un símbolo");
         }
@@ -56,7 +56,7 @@ public static class PasswordValidator
 
     private static void ValidateNoSequence(string password)
     {
-        if (Regex.IsMatch(password, @"012|123|234|345|456|567|678|789"))
+        if(Regex.IsMatch(password, @"012|123|234|345|456|567|678|789"))
         {
             throw new Exception("La contraseña no debe tener secuencias de números");
         }
