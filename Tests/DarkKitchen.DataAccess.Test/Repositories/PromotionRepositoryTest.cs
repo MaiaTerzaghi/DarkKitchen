a8e809284707fd43ea25aa6682cc599614f2b8d5
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-01 02:35:58 -0300

    Red: GetActivePromotions_WhenPromotionIsActive_ReturnsPromotion

---
+++ Tests/DarkKitchen.DataAccess.Test/Repositories/PromotionRepositoryTest.cs
@@ -0,0 +1,56 @@
+using DarkKitchen.DataAccess.Context;
+using DarkKitchen.DataAccess.Repositories;
+using DarkKitchen.Domain.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.DataAccess.Test.Repositories;
+
+[TestClass]
+public sealed class PromotionRepositoryTest
+{
+    private SqliteConnection? _connection;
+    private DarkKitchenContext? _context;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _connection = new SqliteConnection("Data Source=:memory:");
+        _connection.Open();
+
+        var options = new DbContextOptionsBuilder<DarkKitchenContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        _context = new DarkKitchenContext(options);
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context?.Dispose();
+        _connection?.Dispose();
+    }
+
+    [TestMethod]
+    public void GetActivePromotions_WhenPromotionIsActive_ReturnsPromotion()
+    {
+        var promotion = new Promotion
+        {
+            Name = "Black Friday",
+            DiscountPercentage = 10,
+            ValidFrom = new DateTime(2026, 1, 25),
+            ValidTo = new DateTime(2026, 12, 30)
+        };
+
+        _context!.Promotions.Add(promotion);
+        _context.SaveChanges();
+
+        var repository = new PromotionRepository(_context);
+        var result = repository.GetActivePromotions(new DateTime(2026, 4, 1), null, null);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Count);
+    }
+}
