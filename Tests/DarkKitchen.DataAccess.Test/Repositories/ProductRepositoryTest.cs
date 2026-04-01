e5eb9bd33d7001bce16c41142de6bd700fe2b800
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-01 20:19:11 -0300

    GetAll_WhenFilterByCategory_ReturnsFilteredProducts

--- Tests/DarkKitchen.DataAccess.Test/Repositories/ProductRepositoryTest.cs
+++ Tests/DarkKitchen.DataAccess.Test/Repositories/ProductRepositoryTest.cs
@@ -52,4 +52,18 @@ public sealed class ProductRepositoryTest
         Assert.AreEqual(1, result.Count);
         Assert.AreEqual("Pizza", result[0].Name);
     }
+
+    [TestMethod]
+    public void GetAll_WhenFilterByCategory_ReturnsFilteredProducts()
+    {
+        _context!.Products.Add(new Product { Code = "P001", Name = "Pizza", Category = "Fritos", CommercialLine = "Minutas", Description = "Rica pizza", Price = 100 });
+        _context.Products.Add(new Product { Code = "P002", Name = "Pasta", Category = "Pastas", CommercialLine = "Minutas", Description = "Rica pasta", Price = 80 });
+        _context.SaveChanges();
+
+        var repository = new ProductRepository(_context);
+        var result = repository.GetAll(null, "Fritos", null);
+
+        Assert.AreEqual(1, result.Count);
+        Assert.AreEqual("Pizza", result[0].Name);
+    }
 }
