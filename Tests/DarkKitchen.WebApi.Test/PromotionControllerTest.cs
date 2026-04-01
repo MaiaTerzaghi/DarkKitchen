e0f292abfa92e3a3817d7555cb37132d9c368e9e
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-01 02:10:59 -0300

    Red: GetActivePromotions_WhenCalled_ReturnsOk

---
+++ Tests/DarkKitchen.WebApi.Test/PromotionControllerTest.cs
@@ -0,0 +1,24 @@
+using DarkKitchen.BusinessLogic.Interfaces;
+using DarkKitchen.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace DarkKitchen.WebApi.Test;
+
+[TestClass]
+public sealed class PromotionControllerTest
+{
+    [TestMethod]
+    public void GetActivePromotions_WhenCalled_ReturnsOk()
+    {
+        var promotionServiceMock = new Mock<IPromotionService>();
+        promotionServiceMock.Setup(s => s.GetActivePromotions(null, null, null))
+                            .Returns([]);
+
+        var controller = new PromotionController(promotionServiceMock.Object);
+
+        var result = controller.GetActivePromotions(null, null, null);
+
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+    }
+}
