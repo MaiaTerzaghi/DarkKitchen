16e7e91f84dbb0a216045f34ab4c12a133a9dfba
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-06 19:01:04 -0300

    Green: GetActivePromotions_WhenProductFilter_ReturnsFilteredPromotions

--- DarkKitchen.DataAccess/Repositories/PromotionRepository.cs
+++ DarkKitchen.DataAccess/Repositories/PromotionRepository.cs
@@ -1,6 +1,7 @@
 using DarkKitchen.DataAccess.Context;
 using DarkKitchen.Domain.Entities;
 using DarkKitchen.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DarkKitchen.DataAccess.Repositories;
 
@@ -11,8 +12,10 @@ public class PromotionRepository(DarkKitchenContext context) : IPromotionReposit
     public List<Promotion> GetActivePromotions(DateTime? date, string? productLine, string? product)
     {
         return _context.Promotions
+            .Include(p => p.Products)
             .Where(p => !date.HasValue || (p.ValidFrom <= date.Value && p.ValidTo >= date.Value))
             .Where(p => string.IsNullOrEmpty(productLine) || p.ProductLine == productLine)
+            .Where(p => string.IsNullOrEmpty(product) || p.Products.Any(pr => pr.Name == product))
             .ToList();
     }
 }
