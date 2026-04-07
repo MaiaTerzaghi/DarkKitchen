6c0c4fdce9ed6e9f788974d16127b6ea2e2e2b81
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-07 16:45:51 -0300

    cambie el nombre de la carpeta out a output para no tener problemas con .gitignore

--- Tests/DarkKitchen.WebApi.Test/ProductControllerTest.cs
+++ Tests/DarkKitchen.WebApi.Test/ProductControllerTest.cs
@@ -1,4 +1,4 @@
-using DarkKitchen.BusinessLogic.Args.Out;
+using DarkKitchen.BusinessLogic.Args.Output;
 using DarkKitchen.BusinessLogic.Interfaces;
 using DarkKitchen.Domain.Entities;
 using DarkKitchen.WebApi.Controllers;
