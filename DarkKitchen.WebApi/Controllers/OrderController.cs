4c98a361c59172e056e1c155573cdccddb534312
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-07 17:58:29 -0300

    green: CreateOrder_EmptyItems_ReturnsBadRequest

--- DarkKitchen.WebApi/Controllers/OrderController.cs
+++ DarkKitchen.WebApi/Controllers/OrderController.cs
@@ -13,7 +13,14 @@ public class OrderController(IOrderService orderService) : ControllerBase
     [HttpPost("create")]
     public IActionResult CreateOrder([FromBody] CreateOrderRequestDTO request)
     {
-        var response = _orderService.CreateOrder(request);
-        return Ok(response);
+        try
+        {
+            var response = _orderService.CreateOrder(request);
+            return Ok(response);
+        }
+        catch(ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 }
