1c6731d7562c4c6b921b43ad0ecb286be407293f
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-08 15:58:44 -0300

    green: Save_ValidOrder_ReturnsSavedOrderWithId

--- DarkKitchen.DataAccess/Repositories/OrderRepository.cs
+++ DarkKitchen.DataAccess/Repositories/OrderRepository.cs
@@ -6,14 +6,12 @@ namespace DarkKitchen.DataAccess.Repositories;
 
 public class OrderRepository(DarkKitchenContext context) : IOrderRepository
 {
-    // TDD Red: se suprime el warning de campo no utilizado temporalmente.
-    // _context será utilizado en el paso Green cuando  mplemente Save.
-#pragma warning disable CA1823, CS9113
     private readonly DarkKitchenContext _context = context;
-#pragma warning restore CA1823, CS9113
 
     public Order Save(Order order)
     {
-        throw new NotImplementedException();
+        _context.Orders.Add(order);
+        _context.SaveChanges();
+        return order;
     }
 }
