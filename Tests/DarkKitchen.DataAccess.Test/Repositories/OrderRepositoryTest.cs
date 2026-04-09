30be1b0ea9a59d97688f5fc9545cf5783670386d
santibarcos <103213869+santibarcos@users.noreply.github.com>
2026-04-09 20:47:53 -0300

    RED-GetOrders_FilterByDateRange_ReturnsOrdersWithinRange

--- Tests/DarkKitchen.DataAccess.Test/Repositories/OrderRepositoryTest.cs
+++ Tests/DarkKitchen.DataAccess.Test/Repositories/OrderRepositoryTest.cs
@@ -1,3 +1,4 @@
+using DarkKitchen.BusinessLogic.Args.In;
 using DarkKitchen.DataAccess.Context;
 using DarkKitchen.DataAccess.Repositories;
 using DarkKitchen.Domain.Entities;
@@ -53,4 +54,46 @@ public sealed class OrderRepositoryTest
         Assert.IsNotNull(result);
         Assert.AreNotEqual(0, result.Id);
     }
+
+    [TestMethod]
+public void GetOrders_FilterByDateRange_ReturnsOrdersWithinRange()
+{
+    var order1 = new Order
+    {
+        ClientId = 1,
+        DeliveryType = "Express",
+        Status = "Pending",
+        Street = "18 de Julio",
+        DoorNumber = "1234",
+        Date = new DateTime(2026, 1, 10),
+        Items = []
+    };
+
+    var order2 = new Order
+    {
+        ClientId = 2,
+        DeliveryType = "Express",
+        Status = "Pending",
+        Street = "Av. Italia",
+        DoorNumber = "5678",
+        Date = new DateTime(2026, 3, 10),
+        Items = []
+    };
+
+    _context!.Orders.AddRange(order1, order2);
+    _context.SaveChanges();
+
+    var repository = new OrderRepository(_context);
+
+    var request = new GetOrdersRequestDTO
+    {
+        DateFrom = new DateTime(2026, 1, 1),
+        DateTo = new DateTime(2026, 1, 31)
+    };
+
+    var result = repository.GetOrders(request);
+
+    Assert.AreEqual(1, result.Count);
+    Assert.AreEqual(order1.Id, result[0].Id);
+}
 }
