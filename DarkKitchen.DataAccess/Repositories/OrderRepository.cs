091f0bf604b9e93189069e700c7c079c0e1004af
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-09 19:51:04 -0300

    Green: GetClientOrders_WhenClientHasOrders_ReturnsOrders

--- DarkKitchen.DataAccess/Repositories/OrderRepository.cs
+++ DarkKitchen.DataAccess/Repositories/OrderRepository.cs
@@ -2,6 +2,7 @@ using DarkKitchen.BusinessLogic.Args.In;
 using DarkKitchen.BusinessLogic.Interfaces;
 using DarkKitchen.DataAccess.Context;
 using DarkKitchen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DarkKitchen.DataAccess.Repositories;
 
@@ -18,6 +19,13 @@ public class OrderRepository(DarkKitchenContext context) : IOrderRepository
 
     public List<Order> GetClientOrders(GetClientOrdersRequestDTO request)
     {
-        throw new NotImplementedException();
+        return _context.Orders
+            .Include(o => o.Items)
+            .ThenInclude(i => i.Product)
+            .Where(o => o.ClientId == request.ClientId)
+            .Where(o => string.IsNullOrEmpty(request.Status) || o.Status == request.Status)
+            .Where(o => !request.DateFrom.HasValue || o.Date >= request.DateFrom.Value)
+            .Where(o => !request.DateTo.HasValue || o.Date <= request.DateTo.Value)
+            .ToList();
     }
 }
