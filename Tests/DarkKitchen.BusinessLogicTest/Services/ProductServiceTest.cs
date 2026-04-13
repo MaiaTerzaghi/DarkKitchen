ffd7f3a347d0865073df8256f134a498b8aefc9b
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-13 19:45:51 -0300

    Red: CreateProduct_WhenNoImages_ThrowsException

--- Tests/DarkKitchen.BusinessLogicTest/Services/ProductServiceTest.cs
+++ Tests/DarkKitchen.BusinessLogicTest/Services/ProductServiceTest.cs
@@ -292,4 +292,27 @@ public sealed class ProductServiceTest
         var productService = new ProductService(productRepositoryMock.Object);
         productService.CreateProduct(request);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WhenNoImages_ThrowsException()
+    {
+        var request = new CreateProductRequestDTO
+        {
+            Code = "P0001",
+            Name = "Pizza Napolitana",
+            Description = "Rica pizza napolitana con tomate y albahaca",
+            Price = 100.0,
+            CommercialLine = "Minutas",
+            Category = "Fritos",
+            Images = string.Empty
+        };
+
+        var productRepositoryMock = new Mock<IProductRepository>();
+        productRepositoryMock.Setup(r => r.Add(It.IsAny<Product>()))
+                            .Returns(new Product { Code = "P0001", Name = "Pizza Napolitana", Description = "Rica pizza napolitana con tomate", CommercialLine = "Minutas", Category = "Fritos", Images = "pizza.jpg" });
+
+        var productService = new ProductService(productRepositoryMock.Object);
+        productService.CreateProduct(request);
+    }
 }
