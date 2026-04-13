43b58555c19afb232c1498e6d9d828eb3e880d9b
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-13 19:21:16 -0300

    Red: CreateProduct_WhenPriceIsNegative_ThrowsException

--- Tests/DarkKitchen.BusinessLogicTest/Services/ProductServiceTest.cs
+++ Tests/DarkKitchen.BusinessLogicTest/Services/ProductServiceTest.cs
@@ -269,4 +269,27 @@ public sealed class ProductServiceTest
         var productService = new ProductService(productRepositoryMock.Object);
         productService.CreateProduct(request);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WhenPriceIsNegative_ThrowsException()
+    {
+        var request = new CreateProductRequestDTO
+        {
+            Code = "P0001",
+            Name = "Pizza Napolitana",
+            Description = "Rica pizza napolitana con tomate y albahaca",
+            Price = -1.0,
+            CommercialLine = "Minutas",
+            Category = "Fritos",
+            Images = "pizza.jpg"
+        };
+
+        var productRepositoryMock = new Mock<IProductRepository>();
+        productRepositoryMock.Setup(r => r.Add(It.IsAny<Product>()))
+                            .Returns(new Product { Code = "P0001", Name = "Pizza Napolitana", Description = "Rica pizza napolitana con tomate", CommercialLine = "Minutas", Category = "Fritos", Images = "pizza.jpg" });
+
+        var productService = new ProductService(productRepositoryMock.Object);
+        productService.CreateProduct(request);
+    }
 }
