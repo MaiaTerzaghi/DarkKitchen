35bf09853f9064a3d7550789a20cf895ff39265a
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-13 19:17:26 -0300

    Red: CreateProduct_WhenCategoryIsEmpty_ThrowsException

--- Tests/DarkKitchen.BusinessLogicTest/Services/ProductServiceTest.cs
+++ Tests/DarkKitchen.BusinessLogicTest/Services/ProductServiceTest.cs
@@ -246,4 +246,27 @@ public sealed class ProductServiceTest
         var productService = new ProductService(productRepositoryMock.Object);
         productService.CreateProduct(request);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WhenCategoryIsEmpty_ThrowsException()
+    {
+        var request = new CreateProductRequestDTO
+        {
+            Code = "P0001",
+            Name = "Pizza Napolitana",
+            Description = "Rica pizza napolitana con tomate y albahaca",
+            Price = 100.0,
+            CommercialLine = "Minutas",
+            Category = string.Empty,
+            Images = "pizza.jpg"
+        };
+
+        var productRepositoryMock = new Mock<IProductRepository>();
+        productRepositoryMock.Setup(r => r.Add(It.IsAny<Product>()))
+                            .Returns(new Product { Code = "P0001", Name = "Pizza Napolitana", Description = "Rica pizza napolitana con tomate", CommercialLine = "Minutas", Category = "Fritos", Images = "pizza.jpg" });
+
+        var productService = new ProductService(productRepositoryMock.Object);
+        productService.CreateProduct(request);
+    }
 }
