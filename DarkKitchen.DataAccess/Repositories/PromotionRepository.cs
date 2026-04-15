e7272ddb90f2588589f29614605a806dc459f29f
santibarcos <103213869+santibarcos@users.noreply.github.com>
2026-04-15 17:00:21 -0300

    GREEN-productHasActivePromotio

--- DarkKitchen.DataAccess/Repositories/PromotionRepository.cs
+++ DarkKitchen.DataAccess/Repositories/PromotionRepository.cs
@@ -5,13 +5,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DarkKitchen.DataAccess.Repositories;
 
-public class PromotionRepository(DarkKitchenContext context) : Repository<Promotion>(context), IPromotionRepository
+public class PromotionRepository(DarkKitchenContext context)
+    : Repository<Promotion>(context), IPromotionRepository
 {
-    private readonly DarkKitchenContext _context = context;
-
     public List<Promotion> GetActivePromotions(DateTime? date, string? productLine, string? product)
     {
-        return _context.Promotions
+        return context.Promotions
             .Include(p => p.Products)
             .Where(p => !date.HasValue || (p.ValidFrom <= date.Value && p.ValidTo >= date.Value))
             .Where(p => string.IsNullOrEmpty(productLine) || p.ProductLine == productLine)
@@ -21,6 +20,9 @@ public class PromotionRepository(DarkKitchenContext context) : Repository<Promot
 
     public bool ProductHasActivePromotion(int productId)
     {
-        throw new NotImplementedException();
+        return context.Promotions
+            .Any(p => p.ValidFrom <= DateTime.Today
+                && p.ValidTo >= DateTime.Today
+                && p.Products.Any(pr => pr.Id == productId));
     }
 }
