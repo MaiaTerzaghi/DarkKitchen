bf7047ab8056aec1bb52427446730de03e8175d2
santibarcos <103213869+santibarcos@users.noreply.github.com>
2026-04-14 23:50:14 -0300

    GREEN-Add-Repository

--- DarkKitchen.DataAccess/Repositories/Repository.cs
+++ DarkKitchen.DataAccess/Repositories/Repository.cs
@@ -6,12 +6,12 @@ namespace DarkKitchen.DataAccess.Repositories;
 public class Repository<TEntity>(DbContext context) : IRepository<TEntity>
     where TEntity : class
 {
-#pragma warning disable CA1823
     private readonly DbContext _context = context;
     private readonly DbSet<TEntity> _entities = context.Set<TEntity>();
-#pragma warning restore CA1823
     public TEntity Add(TEntity entity)
     {
-        throw new NotImplementedException();
+        _entities.Add(entity);
+        _context.SaveChanges();
+        return entity;
     }
 }
