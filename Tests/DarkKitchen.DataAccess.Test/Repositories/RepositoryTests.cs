17b474eb4db45ab27a95edb977da10459781abb7
santibarcos <103213869+santibarcos@users.noreply.github.com>
2026-04-14 23:47:50 -0300

    RED-Add-Repository

---
+++ Tests/DarkKitchen.DataAccess.Test/Repositories/RepositoryTests.cs
@@ -0,0 +1,54 @@
+using DarkKitchen.DataAccess.Context;
+using DarkKitchen.DataAccess.Repositories;
+using DarkKitchen.Domain.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.DataAccess.Test.Repositories;
+
+[TestClass]
+public sealed class RepositoryTest
+{
+    private SqliteConnection? _connection;
+    private DarkKitchenContext? _context;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _connection = new SqliteConnection("Data Source=:memory:");
+        _connection.Open();
+
+        var options = new DbContextOptionsBuilder<DarkKitchenContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        _context = new DarkKitchenContext(options);
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context?.Dispose();
+        _connection?.Dispose();
+    }
+
+    [TestMethod]
+    public void Add_ValidEntity_ReturnsSavedEntityWithId()
+    {
+        var promotion = new Promotion
+        {
+            Name = "Black Friday",
+            DiscountPercentage = 10,
+            ValidFrom = new DateTime(2026, 1, 25),
+            ValidTo = new DateTime(2026, 1, 30)
+        };
+
+        var repository = new Repository<Promotion>(_context!);
+        var result = repository.Add(promotion);
+
+        Assert.IsNotNull(result);
+        Assert.AreNotEqual(0, result.Id);
+        Assert.AreEqual("Black Friday", result.Name);
+    }
+}
