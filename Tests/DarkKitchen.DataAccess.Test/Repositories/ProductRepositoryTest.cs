125a93ff830e7639d2bddb8a96b89fe94bc85f9e
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-15 01:11:03 -0300

    Red: GetManage_WhenFilterByCommercialLine_ReturnsFilteredProducts

--- Tests/DarkKitchen.DataAccess.Test/Repositories/ProductRepositoryTest.cs
+++ Tests/DarkKitchen.DataAccess.Test/Repositories/ProductRepositoryTest.cs
@@ -276,4 +276,38 @@ public sealed class ProductRepositoryTest
         Assert.AreEqual(1, result.Count);
         Assert.AreEqual("Pizza Napolitana", result[0].Name);
     }
+
+    [TestMethod]
+    public void GetManage_WhenFilterByCommercialLine_ReturnsFilteredProducts()
+    {
+        _context!.Products.Add(new Product
+        {
+            Code = "P0001",
+            Name = "Pizza Napolitana",
+            Description = "Rica pizza napolitana con tomate y albahaca",
+            Price = 100.0,
+            CommercialLine = "Minutas",
+            Category = "Fritos",
+            Images = "pizza.jpg",
+            IsActive = true
+        });
+        _context.Products.Add(new Product
+        {
+            Code = "P0002",
+            Name = "Cafe con leche",
+            Description = "Rico cafe con leche y medialunas",
+            Price = 80.0,
+            CommercialLine = "Desayunos",
+            Category = "Bebidas",
+            Images = "cafe.jpg",
+            IsActive = true
+        });
+        _context.SaveChanges();
+
+        var repository = new ProductRepository(_context);
+        var result = repository.GetManage(new GetProductsManageRequestDTO { CommercialLine = "Minutas" });
+
+        Assert.AreEqual(1, result.Count);
+        Assert.AreEqual("Pizza Napolitana", result[0].Name);
+    }
 }
