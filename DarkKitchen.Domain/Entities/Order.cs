c161ed2d7d679c16e92827d6985f695b41923997
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-04-22 16:08:08 -0300

    green: CreateOrder_WhenStreetIsEmpty_ThrowsException

--- DarkKitchen.Domain/Entities/Order.cs
+++ DarkKitchen.Domain/Entities/Order.cs
@@ -3,12 +3,26 @@ namespace DarkKitchen.Domain.Entities;
 
 public class Order
 {
+    private string _street = string.Empty;
     public int Id { get; set; }
     public int ClientId { get; set; }
     public DeliveryType DeliveryType { get; set; }
     public OrderStatus Status { get; set; }
     public List<OrderItem> Items { get; set; } = [];
-    public string Street { get; set; } = string.Empty;
+    public string Street
+    {
+        get => _street;
+        set
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("La calle no puede estar vacía.");
+            }
+
+            _street = value;
+        }
+    }
+
     public DateTime Date { get; set; } = DateTime.Now;
     public string DoorNumber { get; set; } = string.Empty;
     public string? Apartment { get; set; }
