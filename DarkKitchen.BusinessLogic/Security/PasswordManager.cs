d7401455f1b252a1a437ba92e786c4cd6ee886f1
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-23 15:25:26 -0300

    GREEN:ComputeHash_WhenDifferentInputs_ReturnsDifferentHashes

--- DarkKitchen.BusinessLogic/Security/PasswordManager.cs
+++ DarkKitchen.BusinessLogic/Security/PasswordManager.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using DarkKitchen.IBusinessLogic;
 
 namespace DarkKitchen.BusinessLogic.Security;
@@ -6,6 +8,9 @@ public class PasswordManager : IPasswordManager
 {
     public string ComputeHash(string password)
     {
-        return "hash";
+        using var sha = SHA256.Create();
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+        var hashBytes = sha.ComputeHash(passwordBytes);
+        return Convert.ToBase64String(hashBytes);
     }
 }
