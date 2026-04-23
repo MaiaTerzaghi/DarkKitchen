3a165c9719936257317bf5473a9855956d1715fa
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-23 15:42:32 -0300

    RED: login hashea contrasena antes de comparar

--- Tests/DarkKitchen.BusinessLogicTest/Services/AuthServiceTest.cs
+++ Tests/DarkKitchen.BusinessLogicTest/Services/AuthServiceTest.cs
@@ -12,15 +12,17 @@ public sealed class AuthServiceTest
     [TestMethod]
     public void Login_WhenValidCredentials_ReturnsToken()
     {
+        var hashedPassword = "hashed-contrasena";
         var user = new User
         {
             Id = 1,
             Email = "juan@email.com",
-            Password = "Contrasena1!@#$%"
+            Password = hashedPassword
         };
 
         var userRepositoryMock = new Mock<IRepository<User>>();
         var sessionRepositoryMock = new Mock<ISessionRepository>();
+        var passwordManagerMock = new Mock<IPasswordManager>();
 
         userRepositoryMock.Setup(r => r.Get(It.IsAny<Expression<Func<User, bool>>>()))
                           .Returns(user);
@@ -28,7 +30,10 @@ public sealed class AuthServiceTest
         sessionRepositoryMock.Setup(r => r.Add(It.IsAny<Session>()))
                     .Returns(new Session { User = user });
 
-        var authService = new AuthService(userRepositoryMock.Object, sessionRepositoryMock.Object);
+        passwordManagerMock.Setup(p => p.ComputeHash("Contrasena1!@#$%"))
+                    .Returns(hashedPassword);
+
+        var authService = new AuthService(userRepositoryMock.Object, sessionRepositoryMock.Object, passwordManagerMock.Object);
 
         var result = authService.Login("juan@email.com", "Contrasena1!@#$%");
 
@@ -42,10 +47,12 @@ public sealed class AuthServiceTest
     {
         var userRepositoryMock = new Mock<IRepository<User>>();
         var sessionRepositoryMock = new Mock<ISessionRepository>();
+        var passwordManagerMock = new Mock<IPasswordManager>();
+
         userRepositoryMock.Setup(r => r.Get(It.IsAny<Expression<Func<User, bool>>>()))
                         .Returns((User?)null);
 
-        var authService = new AuthService(userRepositoryMock.Object, sessionRepositoryMock.Object);
+        var authService = new AuthService(userRepositoryMock.Object, sessionRepositoryMock.Object, passwordManagerMock.Object);
 
         authService.Login("mal@test.com", "Contrasena1!@#$%");
     }
