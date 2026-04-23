ea8b606951f3c2a6b4a6049074e8988f12c2dda1
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-23 15:15:12 -0300

    RED: ComputeHash_WhenCalledTwiceWithSameInput_ReturnsSameHash

---
+++ Tests/DarkKitchen.BusinessLogicTest/Security/PasswordManagerTest.cs
@@ -0,0 +1,24 @@
+namespace DarkKitchen.BusinessLogicTest.Security;
+
+[TestClass]
+public sealed class PasswordManagerTest
+{
+    private IPasswordManager _passwordManager = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _passwordManager = new PasswordManager();
+    }
+
+    [TestMethod]
+    public void ComputeHash_WhenCalledTwiceWithSameInput_ReturnsSameHash()
+    {
+        var password = "Contrasena1!@#$%";
+
+        var hash1 = _passwordManager.ComputeHash(password);
+        var hash2 = _passwordManager.ComputeHash(password);
+
+        Assert.AreEqual(hash1, hash2);
+    }
+}
