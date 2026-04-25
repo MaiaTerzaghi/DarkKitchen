1dd5f7f7eeed24d6bed0d691642fd8d44ccedb88
MaiaTerzaghi <maiahiggie@gmail.com>
2026-04-24 21:17:03 -0300

    REFACTOR: centralice constantes de pricing en AppConstants

---
+++ DarkKitchen.Domain/AppConstants.cs
@@ -0,0 +1,8 @@
+namespace DarkKitchen.Domain;
+
+public static class AppConstants
+{
+    public const double Vat = 0.22;
+    public const int TopProductsCount = 5;
+    public const int DefaultPageSize = 20;
+}
