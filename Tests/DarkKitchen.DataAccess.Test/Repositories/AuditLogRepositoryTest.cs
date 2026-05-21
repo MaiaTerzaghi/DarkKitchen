2fa4903c4208d3047d755408b034f294e19ae6ed
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-05-21 01:24:53 -0300

    Red: GetByEntity_WhenMatchesEntityAndRange_ReturnsLog

---
+++ Tests/DarkKitchen.DataAccess.Test/Repositories/AuditLogRepositoryTest.cs
@@ -0,0 +1,59 @@
+using DarkKitchen.DataAccess.Context;
+using DarkKitchen.DataAccess.Repositories;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Enums;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.DataAccess.Test.Repositories;
+
+[TestClass]
+public sealed class AuditLogRepositoryTest
+{
+    private SqliteConnection? _connection;
+    private DarkKitchenContext? _context;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _connection = new SqliteConnection("Data Source=:memory:");
+        _connection.Open();
+
+        var options = new DbContextOptionsBuilder<DarkKitchenContext>()
+            .UseSqlite(_connection)
+            .Options;
+
+        _context = new DarkKitchenContext(options);
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context?.Dispose();
+        _connection?.Dispose();
+    }
+
+    [TestMethod]
+    public void GetByEntity_WhenMatchesEntityAndRange_ReturnsLog()
+    {
+        _context!.AuditLogs.Add(new AuditLog
+        {
+            Timestamp = new DateTime(2026, 4, 23, 9, 0, 0),
+            EntityName = AuditedEntity.Product,
+            EntityId = 12345,
+            Description = "Alta de producto.",
+            ResponsibleUser = "admin@email.com"
+        });
+        _context.SaveChanges();
+
+        var repository = new AuditLogRepository(_context);
+        var result = repository.GetByEntity(
+            AuditedEntity.Product,
+            12345,
+            new DateTime(2026, 4, 23, 8, 0, 0),
+            new DateTime(2026, 4, 23, 10, 0, 0));
+
+        Assert.AreEqual(1, result.Count);
+    }
+}
