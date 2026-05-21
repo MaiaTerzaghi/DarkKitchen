f45a7a1f76809390e857b6156818c2e9205f38cc
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-05-21 00:44:03 -0300

    Red: GetLogs_WhenEntityNameIsMissing_ThrowsArgumentException

--- Tests/DarkKitchen.BusinessLogicTest/Services/AuditServiceTest.cs
+++ Tests/DarkKitchen.BusinessLogicTest/Services/AuditServiceTest.cs
@@ -42,4 +42,18 @@ public sealed class AuditServiceTest
 
         service.GetLogs(request);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void GetLogs_WhenEntityNameIsMissing_ThrowsArgumentException()
+    {
+        var service = new AuditService();
+        var request = new GetAuditLogsRequestDTO
+        {
+            DateFrom = new DateTime(2026, 4, 23, 8, 0, 0),
+            DateTo = new DateTime(2026, 4, 23, 10, 0, 0)
+        };
+
+        service.GetLogs(request);
+    }
 }
