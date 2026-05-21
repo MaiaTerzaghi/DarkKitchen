0c67423db892cbac5939eb0576baaa67fb09def4
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-05-21 01:45:40 -0300

    Green: GetByEntity_WhenMatchesEntityAndRange_ReturnsLog

---
+++ DarkKitchen.DataAccess/Repositories/AuditRepository.cs
@@ -0,0 +1,15 @@
+using DarkKitchen.DataAccess.Context;
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.Domain.Enums;
+using DarkKitchen.IDataAccess;
+
+namespace DarkKitchen.DataAccess.Repositories;
+
+public class AuditLogRepository(DarkKitchenContext context)
+    : Repository<AuditLog>(context), IAuditRepository
+{
+    public List<AuditLog> GetByEntity(AuditedEntity entityName, int entityId, DateTime from, DateTime to)
+    {
+        return context.AuditLogs.ToList();
+    }
+}
