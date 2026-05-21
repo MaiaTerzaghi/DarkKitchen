e1eaac18a9679ad8278cea7f682e1d046e28be10
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-05-21 00:28:27 -0300

    Red: GetLogs_WhenCalled_ReturnsOk

---
+++ Tests/DarkKitchen.WebApi.Test/AuditControllerTest.cs
@@ -0,0 +1,25 @@
+using DarkKitchen.DTOs.Args.In;
+using DarkKitchen.DTOs.Args.Output;
+using DarkKitchen.IBusinessLogic;
+using DarkKitchen.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace DarkKitchen.WebApi.Test;
+
+[TestClass]
+public sealed class AuditControllerTest
+{
+    [TestMethod]
+    public void GetLogs_WhenCalled_ReturnsOk()
+    {
+        var auditServiceMock = new Mock<IAuditService>();
+        auditServiceMock.Setup(s => s.GetLogs(It.IsAny<GetAuditLogsRequestDTO>()))
+            .Returns(new List<AuditLogResponseDTO>());
+        var controller = new AuditController(auditServiceMock.Object);
+
+        var result = controller.GetLogs(new GetAuditLogsRequestDTO());
+
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+    }
+}
