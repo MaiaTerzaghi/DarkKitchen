f1dfa5aafbc66560f00cc663e685346dd701cec7
MaiaTerzaghi <maiahiggie@gmail.com>
2026-05-22 17:47:02 -0300

    creo modelos/repositorio concreto/ service

---
+++ Frontend/src/backend/services/promotion/promotion.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PromotionApiRepositoryService } from '../../repositories/promotion-api-repository.service';
+import PromotionResponse from './models/PromotionResponse';
+import PromotionFilter from './models/PromotionFilter';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PromotionService {
+  constructor(
+    private readonly _repository: PromotionApiRepositoryService
+  ) {}
+
+  public getActivePromotions(
+    filters: PromotionFilter = {}
+  ): Observable<PromotionResponse[]> {
+    return this._repository.getActivePromotions(filters);
+  }
+}
