9dcced55038eaf9d06de81d1ffb45c3fb6925d10
Santi <santiagobarcos2004@gmail.com>
2026-05-26 15:10:57 -0300

    Registro componentes y FormsModule en CustomerModule.

--- Frontend/src/app/customer/customer.module.ts
+++ Frontend/src/app/customer/customer.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerLayoutComponent } from './customer-layout/customer-layout.component';
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
+import { ProductCatalogComponent } from './product-catalog/product-catalog.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
-  declarations: [CustomerLayoutComponent, CustomerDashboardComponent],
-  imports: [CommonModule, CustomerRoutingModule],
+  declarations: [CustomerLayoutComponent, CustomerDashboardComponent, ProductCatalogComponent, CheckoutComponent],
+  imports: [CommonModule, CustomerRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class CustomerModule {}
