753c1c7dbc84ac6ad7d4681f7e26e16205bafe29
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-05-27 00:18:44 -0300

    feat(front): capa de datos del dispatcher (model + repo + service)

--- Frontend/src/backend/repositories/order-api-repository.service.ts
+++ Frontend/src/backend/repositories/order-api-repository.service.ts
@@ -7,6 +7,7 @@ import OrderResponse from '../services/order/models/OrderResponse';
 import OrderFilter from '../services/order/models/OrderFilter';
 import OrderByDateResponse from '../services/order/models/OrderByDateResponse';
 import OrderByDateFilter from '../services/order/models/OrderByDateFilter';
+import OrderStatusResponse from '../services/order/models/OrderStatusResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -50,5 +51,17 @@ export class OrderApiRepositoryService extends ApiRepository {
 
     const query = params.join('&');
     return this.get<OrderByDateResponse[]>('by-date', query);
+  }
+
+  public getDispatcherOrders(): Observable<OrderByDateResponse[]> {
+    return this.get<OrderByDateResponse[]>('dispatcher');
+  }
+
+  public markAsPrepared(id: number): Observable<OrderStatusResponse> {
+    return this.patch<OrderStatusResponse>(id, 'prepared');
+  }
+
+  public deliverOrder(id: number): Observable<OrderStatusResponse> {
+    return this.patch<OrderStatusResponse>(id, 'deliver');
   }
 }
