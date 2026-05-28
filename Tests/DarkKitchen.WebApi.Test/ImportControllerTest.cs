88c405a38445313bb6d881eece19d29c9bb43fb8
Ignacio Braga <JB303018@fi365.ort.edu.uy>
2026-05-27 21:18:36 -0300

    red Import_WhenCalled_ReturnsOkWithImportResult

--- Tests/DarkKitchen.WebApi.Test/ImportControllerTest.cs
+++ Tests/DarkKitchen.WebApi.Test/ImportControllerTest.cs
@@ -1,6 +1,9 @@
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.DTOs.Args.In;
 using DarkKitchen.DTOs.Args.Output;
 using DarkKitchen.IBusinessLogic;
 using DarkKitchen.WebApi.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -28,5 +31,32 @@ public sealed class ImportControllerTest
         Assert.IsInstanceOfType(result, typeof(OkObjectResult));
         var okResult = (OkObjectResult)result;
         Assert.AreSame(importers, okResult.Value);
+    }
+
+    [TestMethod]
+    public void Import_WhenCalled_ReturnsOkWithImportResult()
+    {
+        var request = new ImportProductsRequestDTO
+        {
+            ImporterName = "JSON",
+            Content = "[{ \"code\": \"P0001\" }]",
+            FileName = "products.json"
+        };
+
+        var importResult = new ImportResultDTO { ImportedCount = 1 };
+
+        var importServiceMock = new Mock<IImportService>();
+        importServiceMock.Setup(s => s.Import(request, "admin@email.com"))
+                        .Returns(importResult);
+
+        var controller = new ImportController(importServiceMock.Object);
+        controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext() };
+        controller.HttpContext.Items["RequestingUser"] = new User { Id = 1, Email = "admin@email.com" };
+
+        var result = controller.Import(request);
+
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        var okResult = (OkObjectResult)result;
+        Assert.AreSame(importResult, okResult.Value);
     }
 }
