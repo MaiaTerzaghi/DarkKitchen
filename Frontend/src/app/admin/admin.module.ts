6634722f73526cf4b23344ac4a5a101c5a3c450a
Santi <santiagobarcos2004@gmail.com>
2026-05-29 16:25:39 -0300

    Declaro AdminOrderListComponent en admin module

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 import { BusinessComponentsModule } from '../business-components/business-components.module';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminOrderListComponent } from './admin-order-list/admin-order-list.component';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, AdminDashboardComponent],
-  imports: [CommonModule, AdminRoutingModule, BusinessComponentsModule],
+  declarations: [AdminLayoutComponent, AdminDashboardComponent, AdminOrderListComponent],
+  imports: [CommonModule, AdminRoutingModule, FormsModule, BusinessComponentsModule],
 })
 export class AdminModule {}
