2e57df6da8fc64f690063ebf825b6da85ce5a85a
Santi <santiagobarcos2004@gmail.com>
2026-05-29 16:24:23 -0300

    Agrego rol Administrative al endpoint GetOrders

--- DarkKitchen.WebApi/Controllers/OrderController.cs
+++ DarkKitchen.WebApi/Controllers/OrderController.cs
@@ -31,7 +31,7 @@ public class OrderController(IOrderService orderService) : ControllerBase
         return Ok(orders);
     }
 
-    [AuthorizeRoles(UserRole.Dispatcher)]
+    [AuthorizeRoles(UserRole.Dispatcher, UserRole.Administrative)]
     [HttpGet("by-date")]
     public IActionResult GetOrders([FromQuery] GetOrdersRequestDTO request)
     {
