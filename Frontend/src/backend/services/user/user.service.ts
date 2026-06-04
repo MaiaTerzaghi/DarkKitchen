2f0db337a955f2dd67a95da180d21349a8a4b5f7
MaiaTerzaghi <maiahiggie@gmail.com>
2026-06-04 14:47:51 -0300

    agrego metodos a service y a repo

--- Frontend/src/backend/services/user/user.service.ts
+++ Frontend/src/backend/services/user/user.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserApiRepositoryService } from '../../repositories/user-api-repository.service';
 import RegisterRequest from './models/RegisterRequest';
+import UserResponse from './models/UserResponse';
+import CreateStaffUserRequest from './models/CreateStaffUserRequest';
+import UpdateUserRequest from './models/UpdateUserRequest';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +14,21 @@ export class UserService {
 
   public register(data: RegisterRequest): Observable<number> {
     return this._repository.register(data);
+  }
+
+  public getUsers(name?: string, lastName?: string): Observable<UserResponse[]> {
+    return this._repository.getUsers(name, lastName);
+  }
+
+  public createStaffUser(data: CreateStaffUserRequest): Observable<{ id: number }> {
+    return this._repository.createStaffUser(data);
+  }
+
+  public updateUser(id: number, data: UpdateUserRequest): Observable<UserResponse> {
+    return this._repository.updateUser(id, data);
+  }
+
+  public deleteUser(id: number): Observable<void> {
+    return this._repository.deleteUser(id);
   }
 }
