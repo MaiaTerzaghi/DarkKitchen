e2d2a55f751752eef8dfd168a0a2ee36956e39dd
Santi <santiagobarcos2004@gmail.com>
2026-06-08 17:40:12 -0300

    Cambio el retorno de IRepository a una tupla con el total count

--- DarkKitchen.IDataAccess/IRepository.cs
+++ DarkKitchen.IDataAccess/IRepository.cs
@@ -8,7 +8,7 @@ public interface IRepository<TEntity>
     TEntity? Get(Expression<Func<TEntity, bool>> predicate);
     TEntity Update(TEntity entity);
     void Delete(TEntity entity);
-    List<TEntity> GetAll(
+    (List<TEntity> Items, int TotalCount) GetAll(
     Expression<Func<TEntity, bool>>? predicate = null,
     Expression<Func<TEntity, object>>? orderBy = null,
     bool descending = false,
