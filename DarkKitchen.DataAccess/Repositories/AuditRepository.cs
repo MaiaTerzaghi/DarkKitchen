e9c3e536ea77ec3f8fca184a34889412183ba35e
Santi <santiagobarcos2004@gmail.com>
2026-06-10 15:19:41 -0300

    Implemento Count y Skip/Take en GetByEntity

--- DarkKitchen.DataAccess/Repositories/AuditRepository.cs
+++ DarkKitchen.DataAccess/Repositories/AuditRepository.cs
@@ -8,13 +8,22 @@ namespace DarkKitchen.DataAccess.Repositories;
 public class AuditLogRepository(DarkKitchenContext context)
     : Repository<AuditLog>(context), IAuditRepository
 {
-    public List<AuditLog> GetByEntity(AuditedEntity entityName, int entityId, DateTime from, DateTime to)
+    public (List<AuditLog> Items, int TotalCount) GetByEntity(AuditedEntity entityName, int entityId, DateTime from, DateTime to, int page = 1, int pageSize = 20)
     {
-        return context.AuditLogs
+        var query = context.AuditLogs
             .Where(a => a.EntityName == entityName
                 && a.EntityId == entityId
                 && a.Timestamp >= from
                 && a.Timestamp <= to)
+            .OrderByDescending(a => a.Timestamp);
+
+        var totalCount = query.Count();
+
+        var items = query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
             .ToList();
+
+        return (items, totalCount);
     }
 }
